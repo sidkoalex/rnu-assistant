01f827bfa3ffd58e2bb7336455df14f4ec4929c0
a.sidko <sidkoo@outlook.com>
2020-05-20 10:17:46 +0300

    client_apps | add auto reconnect to kafka

--- client_apps/hello/hello.py
+++ client_apps/hello/hello.py
@@ -1,25 +1,26 @@
 import random
 from json import dumps, loads
+from time import sleep
 
 from kafka import KafkaProducer, KafkaConsumer
 
-producer = KafkaProducer(bootstrap_servers=[f'localhost:29092'])
+producer = None
+consumer = None
+while producer is None and consumer is None:
+    print("Connecting to Kafka...")
+    producer = KafkaProducer(bootstrap_servers=[f'localhost:29092'])
+    consumer = KafkaConsumer('job-searcher',
+                             bootstrap_servers=[f'localhost:29092'],
+                             value_deserializer=lambda x: loads(x.decode('utf-8')))
+    sleep(5)
 
-consumer = KafkaConsumer(
-    'job-searcher',
-    bootstrap_servers=[f'localhost:29092'],
-    value_deserializer=lambda x: loads(x.decode('utf-8')))
+print("Start listening for messages from 'job-searcher' topic")
 
-print("Start reading messages from job-searcher topic")
 for message in consumer:
     payload = message.value
     print("Got message: {}".format(payload))
 
-    if 'job' not in payload:
-        continue
-
-    job = payload['job']
-    if job.get('name') == 'HELLO':
+    if payload['job']['name'] == 'HELLO':
         hello_text = random.choice(['Привет', 'Здравствуйте', 'Рад слышать'])
-        producer.send('say', hello_text)
+        producer.send('say', hello_text.encode())
         print(f"Speaking: {hello_text}")
--- client_apps/voice-synthesis/main.py
+++ client_apps/voice-synthesis/main.py
@@ -1,30 +1,46 @@
-from io import BytesIO
-from json import dumps, loads
+import os
+import pathlib
+from json import dumps
+from os.path import dirname, join
+import time
 
 import requests
 from kafka import KafkaProducer, KafkaConsumer
 from pydub import AudioSegment
 from pydub.playback import play
 
-producer = KafkaProducer(bootstrap_servers=[f'localhost:29092'],
-                         value_serializer=lambda x: dumps(x).encode('utf-8'))
+producer = None
+consumer = None
+while producer is None and consumer is None:
+    print("Connecting to Kafka...")
+    producer = KafkaProducer(bootstrap_servers=[f'localhost:29092'],
+                             value_serializer=lambda x: dumps(x).encode('utf-8'))  # decode json
+    consumer = KafkaConsumer('say',
+                             bootstrap_servers=[f'localhost:29092'],
+                             value_deserializer=lambda x: x.decode('utf-8'))  # decode plain string
+    time.sleep(5)
 
-consumer = KafkaConsumer(
-    'say',
-    bootstrap_servers=[f'localhost:29092'],
-    value_deserializer=lambda x: loads(x.decode('utf-8')))
+print("Start listening messages from 'say' topic")
+
 
 print("Start reading messages from say topic")
 for message in consumer:
     payload = message.value
     print("Got message: {}".format(payload))
 
-    r = requests.get('http://localhost:5050', {
-        'voice': 'Aleksandr',
+    response = requests.get('http://localhost:5050', {
+        'voice': 'Aleksandr',  # russian man voice name
         'text': payload
     })
 
-    # with open('voice.mp3', 'wb') as f:
-    #     f.write(r.content)
+    with open('voice.mp3', 'wb') as f:
+        f.write(response.content)
 
-    play(AudioSegment.from_mp3(BytesIO(r)))
+    song = pyglet.media.load('voice.mp3')
+    song.play()
+    pyglet.app.run()
+
+
+
+
+
